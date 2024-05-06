9b423f70350d5c85da3139edb36d554310d6808b
Demetri-DLL <Demetri.Michael@city.ac.uk>
2024-05-06 01:42:09 +0100

    bloat cleanup

--- imgui-master/examples/example_win32_directx11/main.cpp
+++ imgui-master/examples/example_win32_directx11/main.cpp
@@ -124,8 +124,8 @@ int main(int, char**)
         ImGui::NewFrame();
 
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        //if (show_demo_window)
+            //ImGui::ShowDemoWindow(&show_demo_window);
         
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
@@ -135,9 +135,9 @@ int main(int, char**)
             ImGui::Begin("Ultimate Windows Utility!", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);                          // Create a window called "Hello, world!" and append into it.
  
             ImGui::SetWindowPos(ImVec2(0, 0));
-            ImPlot::CreateContext();
-            ImPlot::ShowDemoWindow();
-            ImPlot::DestroyContext();
+           //ImPlot::CreateContext();
+           //ImPlot::ShowDemoWindow();
+           //ImPlot::DestroyContext();
 
             ImVec4* colors = ImGui::GetStyle().Colors;
             colors[ImGuiCol_PopupBg] = ImColor(0, 0, 0, 0);
@@ -271,7 +271,7 @@ int main(int, char**)
             ImGui::Separator();
 
             ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
             //ImGui::Checkbox("Another Window", &show_another_window);
 
             //ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
@@ -279,12 +279,10 @@ int main(int, char**)
 
             //////////////////////////////////////////
 
-            //test1->GetOSTempFold();
-            //test1->GetWindowsTempFold();
 
             /////////////////////////////////////////////
 
-            ImGui::ShowStyleEditor();
+            //ImGui::ShowStyleEditor();
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
             ImGui::End();
         }
